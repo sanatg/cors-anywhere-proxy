de17d76253b15d1116ce35059f7b42e3a686821d
Sanat Gupta <62789975+sanatg@users.noreply.github.com>
2023-05-22 23:11:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-import { createServer } from "cors-anywhere"
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
 
-const host = process.env.HOST || "0.0.0.0"
-const port = process.env.PORT || 8080
-
-createServer().listen(port, host, () => {
-  console.log(`Running on ${host}:${port}`)
-})
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
